4634f04ec6224c2cec9b4c6bf053cb8f0bf406dc
sakha <reaperdedsec175@gmail.com>
2025-04-26 11:03:34 +0700

    commit pertama

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col item-center justify-center p-8 bg-[#f5f0e6] text-[#7a5c20]">
+      <h1 clasName="text-4xl font-bold mb-4">PUREVERS</h1>
+      <p className="text-lg text-center max-w-xl">
+         Solusi Alami, Inovatif, Dan Berkelanjutan Untuk Kehidupan Masa Depan.
+      </p>
+      </main>
+  );
+}
