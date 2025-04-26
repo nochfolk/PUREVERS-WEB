86f877c60221bf1cbc67c7ca6ca7c6fc91dca068
sakha <reaperdedsec175@gmail.com>
2025-04-26 11:06:01 +0700

    penambahan code

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col item-center justify-center p-8 bg-[#f5f0e6] text-[#7a5c20]">
       <h1 clasName="text-4xl font-bold mb-4">PUREVERS</h1>
       <p className="text-lg text-center max-w-xl">
-         Solusi Alami, Inovatif, Dan Berkelanjutan Untuk Kehidupan Masa Depan.
+         Solusi Alami, Inovatif, Dan Berkelanjutan Untuk Kehidupan Masa Depan Bersama .......
       </p>
       </main>
   );
